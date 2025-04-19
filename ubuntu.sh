061aedb5b50ea556b252c0bc2eb02d56d3f0b3bb
David Canaday <51459757+nerd8622@users.noreply.github.com>
2025-04-19 03:20:58 -0600

    ☺

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,8 +18,8 @@ ufw default deny outgoing
 
 ufw allow in proto tcp from any to any port 20,21
 ufw allow out proto tcp from any to any port 20,21
-#ufw allow in proto tcp from any to any port 30000:31000
-#ufw allow out proto tcp from any to any port 30000:31000
+ufw allow in proto tcp from any to any port 30000:31000
+ufw allow out proto tcp from any to any port 30000:31000
 
 ufw status
 
@@ -32,15 +32,20 @@ for user in $(awk -F: '$3 > 0 && $3 < 1000 && $1 != "nobody" {print $1}' /etc/pa
     [[ "$user" != "sshd" && "$user" != "chrony" && "$user" != "rpc" && "$user" != "rpcuser" && "$user" != "ftp" ]] && userdel -r "$user" 2>/dev/null
 done
 
-passwd -l root || { echo "Failed to lock root!"; exit 1; }
+passwd -l root || { echo "Failed to lock root!"; }
 
-echo "root:NewPassword123!" | chpasswd || { echo "Failed to change root password."; exit 1; }
-echo "sysadmin:NewPassword123!" | chpasswd || { echo "Failed to change sysadmin password."; exit 1; }
+echo "root:NewPassword123!" | chpasswd || { echo "Failed to change root password."; }
+echo "sysadmin:NewPassword123!" | chpasswd || { echo "Failed to change sysadmin password."; }
 
-passwd -S root | grep -q "locked" || { echo "Root not locked."; exit 1; }
+passwd -S root | grep -q "locked" || { echo "Root not locked."; }
 
 echo "Remaining User List:"
 awk -F: '{print $1}' /etc/passwd
+
+echo "Perms FTP"
+chown ftp:ftp  /var/ftp/OreCTF-Simple-Logo.png
+chmod 400 /var/ftp/OreCTF-Simple-Logo.png
+chmod 500 /var/ftp/
 
 echo "OSSEC!"
 wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
